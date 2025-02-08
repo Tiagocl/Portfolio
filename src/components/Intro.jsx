176924d3231c4ba418844a3dd5b3cd20f912f559
Tiagocl <up202004374@up.pt>
2025-02-08 02:38:08 +0000

    background done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import StarsCanvas from './Stars'
-
+import React from "react";
+import StarsCanvas from "./Stars";
 
 const Intro = () => {
   return (
-    <div className='flex h-screen items-center justify-center' >
-    <StarsCanvas />
-    <div>Main page</div>
+    <div className="h-screen w-screen relative overflow-hidden">
+      <StarsCanvas />
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 z-[-2] absolute top-10 w-full h-full object-cover -translate-y-1/2"
+      >
+        <source src="src/assets/blackhole.webm" type="video/webm" />
+      </video>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
