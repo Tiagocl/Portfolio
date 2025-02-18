63816ab764e2010e542b774454e9838d1a6bb98d
Tiagocl <up202004374@up.pt>
2025-02-18 16:56:51 +0000

    only see sentence once

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,40 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import Motivation from "./Pages/Motivation";
 import StarsCanvas from "./components/Stars";
 import Portfolio from "./Pages/Portfolio";
 import { motion, AnimatePresence } from "framer-motion";
 
-
-
 const App = () => {
-  const [showIntro, setShowIntro] = useState(true);
-  const [showMainpage, setShowMainpage] = useState(false);
-
+  const [showIntro, setShowIntro] = useState(() => {
+    return sessionStorage.getItem("seenIntro") !== "true"; // Check sessionStorage
+  });
+  const [showMainpage, setShowMainpage] = useState(!showIntro);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowIntro(false);
-      setTimeout(() => setShowMainpage(true), 500);
-    }, 3500);
-    return () => clearTimeout(timer);
-  }, []);
+    if (showIntro) {
+      const timer = setTimeout(() => {
+        setShowIntro(false);
+        setTimeout(() => {
+          setShowMainpage(true);
+          sessionStorage.setItem("seenIntro", "true"); // Store in sessionStorage
+        }, 500);
+      }, 3500);
+      return () => clearTimeout(timer);
+    }
+  }, [showIntro]);
 
   return (
-
     <div className="background-text">
-      <AnimatePresence >
+      <AnimatePresence>
         {showIntro && (
           <motion.div
-            className=" relative z-0 w-screen h-screen flex justify-center items-center text-3xl font-Inter background-text"
+            className="relative z-0 w-screen h-screen flex justify-center items-center text-3xl font-Inter background-text"
             key="intro"
-            exit={{
-              opacity: 0,
-             
-            }}
-            transition={{
-              duration: 0.5,
-
-            }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
           >
             <Motivation />
             <StarsCanvas />
-
           </motion.div>
         )}
       </AnimatePresence>
@@ -46,23 +42,16 @@ const App = () => {
       <AnimatePresence>
         {showMainpage && (
           <motion.div
-            className="w-screen h-screen "
+            className="w-screen h-screen"
             key="portfolio"
-            initial={{
-              opacity: 0,
-            }}
-            animate={{
-              opacity: 1,
-            }}
-            transition={{
-              duration: 4,
-            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 4 }}
           >
             <Portfolio />
           </motion.div>
         )}
       </AnimatePresence>
-
     </div>
   );
 };
