8430856856887fc102f42bf803b548bfd37dc357
Tiagocl <up202004374@up.pt>
2025-02-25 10:18:46 +0000

    some tweaks

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,14 +1,14 @@
 import { div } from 'framer-motion/client'
 import React from 'react'
-import StarsCanvas
- from '../components/Stars'
+import StarsCanvas from '../components/Stars'
+import PurpleTag from '../components/PurpleTag'
+
 const Skills = () => {
   return (
     <div className='relative h-screen w-screen overflow-hidden'>
         <StarsCanvas />
-        <div className='h-full w-full flex justify-center items-center'>
-            
-            <h1>Skills</h1>
+        <div className='h-full w-full flex flex-col justify-start items-center mt-30'>
+          <PurpleTag  text="About me" width="120"/>
         </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { div } from 'framer-motion/client';
-
+import PurpleTag from './PurpleTag';
 
 const Hero = () => {
 
@@ -9,12 +9,7 @@ const Hero = () => {
   return (
     <div className='relative z-20 flex justify-center items-center w-full h-80 transform translate-y-[35vh] xl:translate-y-[32vh]'>
       <div className="flex flex-col h-80 justify-start items-center gap-5  ">
-        <div className=" shadowInset flex bg-[#040014] w-56 py-[8px] pr-[7px] pl-[10px] rounded-[32px] items-center border border-[#7042f88b] opacity-[0.9] ">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#b49bff" class="bi bi-stars mr-[10px]" viewBox="0 0 16 16">
-            <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
-          </svg>
-          <h1 className=" gradient-text text-[13px] m-0 p-0">Frontend Developer Portfolio</h1>
-        </div>
+        <PurpleTag text="Frontend Developer Portfolio" width="224" />
         <div className="w-[700px] h-[165px] text-[#e5e7eb] text-[50px] leading-13 text-center
       lg:text[50px] 
       xl:text-[57px] xl:h-[200px] xl:w-full xl:leading-15 2xl:text-[65px] 2xl:h-[350px] 2xl:w-full 2xl:leading-18"
